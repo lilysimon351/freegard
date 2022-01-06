624015555cb06c3352503ec89dcc6016d27f47e5
Shushanik Simonyan <43413082+lilysimon351@users.noreply.github.com>
2022-01-06 14:53:11 +0400

    clear css from unused styles; change images places

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const questoions = [
     ],
     images = [
         ['1.jpg', '2.jpg'],
-        ['3.jpg', '4.jpg ', '5.jpg ', '6.jpg'],
+        ['3.jpg', '6.jpg ', '4.jpg ', '5.jpg'],
         ['7.jpg ', '8.jpg ', '9.jpg ', '10.png'],
         []
     ],
